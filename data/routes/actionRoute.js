a93a62c9597de2d7656cbd41488493a596e4785d
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-04-06 12:50:13 -0500

    GET All / GET ID

--- data/routes/actionRoute.js
+++ data/routes/actionRoute.js
@@ -5,8 +5,23 @@ const db = require('../helpers/actionModel.js');
 
 // Server
 router.get('/', (req, res) => {
+  // GET All
   db
     .get()
+    .then(action => {
+      res.json(action);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  // GET ID
+  const { id } = req.params;
+
+  db
+    .get(id)
     .then(action => {
       res.json(action);
     })
--- data/routes/projectRoute.js
+++ data/routes/projectRoute.js
@@ -5,8 +5,23 @@ const db = require('../helpers/projectModel.js');
 
 // Server
 router.get('/', (req, res) => {
+  // GET All
   db
     .get()
+    .then(project => {
+      res.json(project);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  // GET ID
+  const { id } = req.params;
+
+  db
+    .get(id)
     .then(project => {
       res.json(project);
     })
